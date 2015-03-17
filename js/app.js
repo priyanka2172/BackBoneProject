cf28ef87c2bfaccdbb15acac7ae4d3e5b20dbaf8
Priyanka Acharya <priyanka2172@gmail.com>
2015-03-17 14:49:37 -0700

    updated with search filter
    updated

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
     //define product model
     var Contact = Backbone.Model.extend({
         defaults: {
-            photo: "/img/placeholder.png"
+            photo: "/img/placeholder.jpeg"
         }
     });
 
@@ -44,7 +44,11 @@
         initialize: function () {
             this.collection = new Directory(contacts);
             this.render();
+            this.$el.find("#filter").append(this.createSelect());
+            this.on("change:filterType", this.filterByType, this);
+            this.collection.on("reset", this.render, this);
         },
+
 
         render: function () {
             var that = this;
@@ -58,10 +62,75 @@
                 model: item
             });
             this.$el.append(contactView.render().el);
+        },
+        getTypes: function () {
+            return _.uniq(this.collection.pluck("type"), false, function (type) {
+                return type.toLowerCase();
+            });
+        },
+
+        createSelect: function () {
+            var filter = this.$el.find("#filter"),
+                select = $("<select/>", {
+                    html: "<option>All</option>"
+                });
+
+            _.each(this.getTypes(), function (item) {
+                var option = $("<option/>", {
+                    value: item.toLowerCase(),
+                    text: item.toLowerCase()
+                }).appendTo(select);
+            });
+            return select;
+        },
+        events: {
+            "change #filter select": "setFilter"
+        },
+        setFilter: function (e) {
+            this.filterType = e.currentTarget.value;
+            this.trigger("change:filterType");
+        },
+
+        setSelected: function (filterType) {
+            $('option').each(function () {
+                if ($(this).val() === filterType) {
+                    $(this).attr("selected", "selected");
+                } else {
+                    $(this).removeAttr("selected");
+                }
+            });
+        },
+
+// call the function in the filterByType, at the end after allt he if-else logic,
+// because it needs to be called anyway:
+
+        filterByType: function () {
+            setSelected(filterType);
+        }
+    });
+
+
+
+
+
+
+
+
+
+    var ContactsRouter = Backbone.Router.extend({
+        routes: {
+            "filter/:type": "urlFilter"
+        },
+
+        urlFilter: function (type) {
+            directory.filterType = type;
+            directory.trigger("change:filterType");
         }
     });
 
     //create instance of master view
     var directory = new DirectoryView();
+    var contactsRouter = new ContactsRouter();
 
 } (jQuery));
+Backbone.history.start();
